9736ca75ad8eb0571c97f1b917702875ae0c0a6d
valeria <ipz221_gvi@student.ztu.edu.ua>
2024-02-01 12:27:21 +0200

    Add file ConsoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        float num1 = GetValidFloat("Write first num:");
+        float num2 = GetValidFloat("Write second num:");
+
+        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+    }
+
+    static float GetValidFloat(string prompt)
+    {
+        bool isValid = false;
+        float number = 0;
+
+        while (!isValid)
+        {
+            Console.WriteLine(prompt);
+            isValid = float.TryParse(Console.ReadLine(), out number);
+
+            if (!isValid)
+                Console.WriteLine("It's not a number");
+        }
+
+        return number;
+    }
+}
+
